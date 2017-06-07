c0c250ec635f44d295d7824c0ea60e36e2365421
YokingJS <yezhen.ye@163.com>
2017-06-07 21:33:14 +0800

    Update yuJS.js
    添加$$.isArray

--- yuJS.js
+++ yuJS.js
@@ -1,32 +1,101 @@
 (function (g,$,undefined) {
     //yuJS是一个全局方法库//实现了对当前项目的零耦合
     //jsonParse 一个全局parse方法
+
+    //domReady 当前页面全部加载完执行参数中方法
     //screenWidth 获取当前屏幕可视区宽度
     //setEmFun 针对手机端设置rem或者em的基础值
+
     //deleteEmptyNode 删除节点中空子节点、注释子节点、换行或者空格子节点
+    //appendChilds 给父元素添加多个子元素
     //prevNode 取当前元素上一个节点
     //nextNode 取当前节点下一个节点
+    //ancestorNode获取祖先元素
+    //nodeDescendant 只有第一个参数：获取某个元素下所有子元素，返回数组 存在第二个参数：获取某个元素下符合条件的子元素
     //childIndex 当前元素是父元素的第几个元素
+
+    //addClass 元素添加某个class
+    //minusClass 元素减去某个class
     //getDomCss 取节点某个属性
+    //setOpacity 设置元素透明度
+    //addTransition元素添加transition属性
+    //rotate旋转元素
     //show 使用css3 transition实现展开动作
     //hide 使用css3 transition实现收缩动作
-    //domReady 当前页面全部加载完执行参数中方法
-    //appendChilds 给父元素添加多个子元素
-    //ajax$ 实现对ajax的封装
-    //getQueryString 获取当前url的后缀参数//调用方法不传参数则以数组形式取出所有后缀//调用参数为number类型（索引值）则根据索引取后缀/string则根据名称返回
-    //setOpacity 设置元素透明度
     //fadein元素淡入淡出
     //fadeout元素淡入淡出
-    //rotate旋转元素
-    //addClass 元素添加某个class
-    //addTransition元素添加transition属性
-    //minusClass 元素减去某个class
+
+    //ajax$ 实现对ajax的封装
+    //getQueryString 获取当前url的后缀参数//调用方法不传参数则以数组形式取出所有后缀//调用参数为number类型（索引值）则根据索引取后缀/string则根据名称返回
+
     //scroll 元素添加滚动事件
     //pause延时方法
+    //isArray是否是数组
     //setCookie添加cookie，默认有效期365天
     //delCookie删除coolie
+    //isArray 是否是数组
     var yuJS={
         jsonParse : g.JSON && JSON.parse ? JSON.parse : eval,
+     /////////////////////////////////////////////////////////////页面样式,页面运算相关
+        domReady:function(funList){
+            var isReady=false;
+            var readyList= [].concat(funList);
+            var timer;
+
+            var onDOMReady=function(){
+                for(var i=0;i< readyList.length;i++)
+                {
+                    readyList[i].apply(g);
+                }
+                readyList = null;
+            }
+
+            var bindReady = function(evt)
+            {
+                if(isReady) return;
+                isReady=true;
+                onDOMReady.call(g);
+                if($.removeEventListener)
+                {
+                    $.removeEventListener("DOMContentLoaded", bindReady, false);
+                }
+                else if($.attachEvent)
+                {
+                    $.detachEvent("onreadystatechange", bindReady);
+                    if(g == g.top){
+                        clearInterval(timer);//事件发生后清除定时器
+                        timer = null;
+                    }
+                }
+            };
+
+            if($.addEventListener){
+                $.addEventListener("DOMContentLoaded", bindReady, false);
+            }
+            else if($.attachEvent)//非最顶级父窗口
+
+            {
+                $.attachEvent("onreadystatechange", function(){
+                    if((/loaded|complete/).test($.readyState))
+                        bindReady();
+                });
+
+                if(g == g.top)
+                {
+                    timer = setInterval(function(){
+                        try
+                        {
+                            isReady||g.g.doScroll('left');//在IE下用能否执行doScroll判断 dom是否加载完毕
+                        }
+                        catch(e)
+                        {
+                            return;
+                        }
+                        bindReady();
+                    },5);
+                }
+            }
+        },
         screenWidth:function (dom) {
             dom=dom||$;
             return dom.body.offsetWidth;
@@ -42,6 +111,7 @@
             this.actualPx =  this.screenWidth(dom)*unitMatrix / UIWidth;
             dom.documentElement.style.fontSize = this.actualPx + "px";
         },
+     ////////////////////////////////////////////////////////////////////元素节点的运算
         deleteEmptyNode:function (elem){
             var newElem=elem;
             var elem_child = newElem.childNodes;
@@ -52,6 +122,12 @@
                 }
             }
             return newElem;
+        },
+        appendChilds:function (father,sonList) {
+            var list=sonList||[];
+            for(var i in list){0
+                father.appendChild(list[i]);
+            }
         },
         prevNode:function (elem) {
             var $parent=yuJS.deleteEmptyNode(elem.parentNode);
@@ -70,6 +146,58 @@
             newElem=getNext(elem);
             return newElem;
         },
+        ancestorNode:function (elem,upward) {
+            upward=parseInt(upward);
+            if(upward){
+                var $ancestor=elem;
+                while(upward){
+                    $ancestor=$ancestor.parentNode;
+                    upward--;
+                }
+                return $ancestor;
+            }
+            return null;
+        },
+        nodeDescendant:function (elem,selector) {
+            if(!elem)return;
+            //取所有子元素
+            var arr=[];
+            var done=function(node){
+                if(node.children && node.children.length){
+                    var childrenNodes=node.children;
+                    for(var i=0,l=childrenNodes.length;i<l;i++){
+                        arr.push(childrenNodes[i]);
+                        done(childrenNodes[i]);
+                    }
+                }
+            }
+            done(elem);
+            if(!selector) return arr;
+            //根据选择器第一个字符判断是什么类型的选择器,然后匹配元素并返回匹配元素
+            var matchNodes=[];
+            switch (selector.charAt(0)){
+                case '.':
+                    for(var i=0,l=arr.length;i<l;i++){
+                        var className=arr[i].getAttribute('class');
+                        if(className && className.match(selector.substr(1,selector.length-1))){
+                            matchNodes.push(arr[i]);
+                        }
+                    }
+                    if(matchNodes[0])return matchNodes;
+                    break;
+                case '#':
+                    for(var i=0,l=arr.length;i<l;i++){
+                        var idName=arr[i].getAttribute('id');
+                        if(idName && idName.match(selector.substr(1,selector.length-1))){
+                            matchNodes.push(arr[i]);
+                            break;
+                        }
+                    }
+                    if(matchNodes[0])return matchNodes[0];
+                    break;
+                default:break;
+            }
+        },
         childIndex:function (elem) {
             if(!(elem instanceof HTMLElement))return;
             var index=null;
@@ -78,6 +206,16 @@
                 if(childList[i]===elem)index=i;
             }
             return index;
+        },
+     /////////////////////////////////////////////////////////////////////元素样式操作
+        addClass:function (ele,newClass) {
+            if(ele.className.match(newClass))return;
+            ele.className+=(" "+newClass);
+        },
+        minusClass:function (ele,className) {
+            if(!ele.className.match(className))return;
+            var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+            ele.className = ele.className.replace(reg, ' ');
         },
         getDomCss:function(elem,name){
             if(elem.nodeName=='#text')return '0';
@@ -101,6 +239,48 @@
                 if(style) return style;
                 else return '0';
             }
+        },
+        setOpacity:function(ele, opacity) {
+            if (ele.style.opacity != undefined) {
+                ///兼容FF和GG和新版本IE
+                ele.style.opacity = opacity / 100;
+            } else {
+                ///兼容老版本ie
+                ele.style.filter = "alpha(opacity=" + opacity + ")";
+            }
+        },
+        addTransition:function (ele,attr,duration,timing) {
+            var style=(ele && ele.style)||ele;
+            if(!!ele && !attr){
+                style.transition='';
+                style.webkitTransition='';
+                style.oTransition='';
+                style.mozTransition='';
+                return;
+            }
+            timing=timing||'ease-in-out';
+            duration=duration||'500ms';
+            attr=attr||'all';
+            style.transition=attr+' '+duration +' '+timing ;
+            style.webkitTransition=attr+' '+duration +' '+timing;
+            style.oTransition=attr+' '+duration+' '+timing;
+            style.mozTransition=attr+' '+duration+' '+timing;
+        },
+        //DEMO
+        // $$.rotate(elem,{
+        //     deg:0
+        // });
+        rotate:function (elem,options) {
+            if(!elem || elem.style==undefined || !options)return;
+            var style=elem.style;
+            var duration=parseInt(options.duration ||300)+'ms';
+            var timing=options.timing||'ease';
+            style.transition='transform '+duration +' '+timing ;
+            style.webkitTransition='transform '+duration +' '+timing ;
+            style.oTransition='transform '+duration +' '+timing ;
+            style.mozTransition='transform '+duration +' '+timing ;
+            style.transform='rotate('+options.deg+'deg)';
+            style.transformOrigin=options.origin||'center center';
         },
         //     $$.show DEMO
         //     $$.show(elem,{
@@ -173,7 +353,7 @@
                 parentOH=attObj.height==='const'?POH+'px':PS.height;
                 parentOverflow=this.getDomCss($parent,'overflow');
                 PS.height=$parent.offsetHeight+'px';
-                PS.overflow = 'hidden';
+                //PS.overflow = 'hidden';
                 style.display='block';
             }
             var oHeight;
@@ -250,14 +430,6 @@
             timingStyle=timingStyle||'ease-in-out';
             var isNum=typeof(timingStyle)==='number';
             var isNumStr=parseInt(timingStyle)>0;
-            // if(elem.parentNode){
-            //     var $parent=elem.parentNode;
-            //     parentOH=$parent.style.height;
-            //     parentOverflow=yuJS.getDomCss($parent,'overflow');
-            //     $parent.style.height=$parent.offsetHeight+'px';
-            //     $parent.style.overflow = 'hidden';
-            //     style.display='block';
-            // }
             var oHeight;
             var oPaddingT;
             var oPaddingB;
@@ -288,71 +460,47 @@
                 },parseInt(duration));
             },-1);
         },
-        domReady:function(funList){
-            var isReady=false;
-            var readyList= [].concat(funList);
-            var timer;
+        fadein:function(ele, opacity, time,funBack) {
+            if (ele) {
+                var v  = ele.style.filter.replace("alpha(opacity=", "").replace(")", "") || ele.style.opacity;
+                v < 1 && (v = v * 100);
+                if(v>opacity) return;
+                var oldV=v;
 
-            var onDOMReady=function(){
-                for(var i=0;i< readyList.length;i++)
-                {
-                    readyList[i].apply(g);
-                }
-                readyList = null;
-            }
-
-            var bindReady = function(evt)
-            {
-                if(isReady) return;
-                isReady=true;
-                onDOMReady.call(g);
-                if($.removeEventListener)
-                {
-                    $.removeEventListener("DOMContentLoaded", bindReady, false);
-                }
-                else if($.attachEvent)
-                {
-                    $.detachEvent("onreadystatechange", bindReady);
-                    if(g == g.top){
-                        clearInterval(timer);//事件发生后清除定时器
-                        timer = null;
+                var count = 2;
+                var timer = null;
+                timer = setInterval(function() {
+                    if (v < opacity) {
+                        v += count;
+                        $$ && $$.setOpacity(ele, v>opacity?opacity:v);
+                    } else {
+                        clearInterval(timer);
+                        funBack && funBack();
                     }
-                }
-            };
-
-            if($.addEventListener){
-                $.addEventListener("DOMContentLoaded", bindReady, false);
-            }
-            else if($.attachEvent)//非最顶级父窗口
-
-            {
-                $.attachEvent("onreadystatechange", function(){
-                    if((/loaded|complete/).test($.readyState))
-                        bindReady();
-                });
-
-                if(g == g.top)
-                {
-                    timer = setInterval(function(){
-                        try
-                        {
-                            isReady||g.g.doScroll('left');//在IE下用能否执行doScroll判断 dom是否加载完毕
-                        }
-                        catch(e)
-                        {
-                            return;
-                        }
-                        bindReady();
-                    },5);
-                }
+                }, time/Math.floor((opacity-oldV)/count));
             }
         },
-        appendChilds:function (father,sonList) {
-            var list=sonList||[];
-            for(var i in list){0
-                father.appendChild(list[i]);
+        fadeout:function(ele, opacity, time) {
+            if (ele) {
+                var v = ele.style.filter.replace("alpha(opacity=", "").replace(")", "") || ele.style.opacity || 100;
+                v <= 1 && (v = v * 100);
+                if(v<opacity) return;
+                var oldV=v;
+
+                var count =2;
+                var avg = (100 - opacity) / count;
+                var timer = null;
+                timer = setInterval(function() {
+                    if (v  > opacity) {
+                        v -= count;
+                        $$ && $$.setOpacity(ele, v<opacity?opacity:v);
+                    } else {
+                        clearInterval(timer);
+                    }
+                }, time/Math.floor((oldV-opacity)/count));
             }
         },
+     ////////////////////////////////////////////////////////////////////其他公共方法
         ajax$:function ajax(opt) {
             opt = opt || {};
             opt.method = opt.method.toUpperCase() || 'POST';
@@ -412,97 +560,6 @@
                 result = result.substring(startIndex);
                 return result;
             }
-        },
-        setOpacity:function(ele, opacity) {
-            if (ele.style.opacity != undefined) {
-                ///兼容FF和GG和新版本IE
-                ele.style.opacity = opacity / 100;
-            } else {
-                ///兼容老版本ie
-                ele.style.filter = "alpha(opacity=" + opacity + ")";
-            }
-        },
-        fadein:function(ele, opacity, time,funBack) {
-            if (ele) {
-                var v  = ele.style.filter.replace("alpha(opacity=", "").replace(")", "") || ele.style.opacity;
-                v < 1 && (v = v * 100);
-                if(v>opacity) return;
-                var oldV=v;
-
-                var count = 2;
-                var timer = null;
-                timer = setInterval(function() {
-                    if (v < opacity) {
-                        v += count;
-                        $$ && $$.setOpacity(ele, v>opacity?opacity:v);
-                    } else {
-                        clearInterval(timer);
-                        funBack && funBack();
-                    }
-                }, time/Math.floor((opacity-oldV)/count));
-            }
-        },
-        fadeout:function(ele, opacity, time) {
-            if (ele) {
-                var v = ele.style.filter.replace("alpha(opacity=", "").replace(")", "") || ele.style.opacity || 100;
-                v <= 1 && (v = v * 100);
-                if(v<opacity) return;
-                var oldV=v;
-
-                var count =2;
-                var avg = (100 - opacity) / count;
-                var timer = null;
-                timer = setInterval(function() {
-                    if (v  > opacity) {
-                        v -= count;
-                        $$ && $$.setOpacity(ele, v<opacity?opacity:v);
-                    } else {
-                        clearInterval(timer);
-                    }
-                }, time/Math.floor((oldV-opacity)/count));
-            }
-        },
-        //DEMO
-        // $$.rotate(elem,{
-        //     deg:0
-        // });
-        rotate:function (elem,options) {
-            if(!elem || elem.style==undefined || !options)return;
-            var style=elem.style;
-            var duration=parseInt(options.duration ||300)+'ms';
-            var timing=options.timing||'ease';
-            style.transition='transform '+duration +' '+timing ;
-            style.webkitTransition='transform '+duration +' '+timing ;
-            style.oTransition='transform '+duration +' '+timing ;
-            style.mozTransition='transform '+duration +' '+timing ;
-            style.transform='rotate('+options.deg+'deg)';
-            style.transformOrigin='center center';
-        },
-        addClass:function (ele,newClass) {
-            if(ele.className.match(newClass))return;
-            ele.className+=(" "+newClass);
-        },
-        addTransition:function (ele,attr,duration,timing) {
-            var style=(ele && ele.style)||ele;
-            if(!!ele && !attr){
-                style.transition='';
-                style.webkitTransition='';
-                style.oTransition='';
-                style.mozTransition='';
-                return;
-            }
-            timing=timing||'ease-in-out';
-            duration=duration||'500ms';
-            attr=attr||'all';
-            style.transition=attr+' '+duration +' '+timing ;
-            style.webkitTransition=attr+' '+duration +' '+timing;
-            style.oTransition=attr+' '+duration+' '+timing;
-            style.mozTransition=attr+' '+duration+' '+timing;
-        },
-        minusClass:function (ele,className) {
-            if(!ele.className.match(className))return;
-            var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
-            ele.className = ele.className.replace(reg, ' ');
         },
         //@param obj parse//parse.$box监听元素//parse.dir XY方向//parse.posType移动元素相关参数//parse.pre是否阻止其他事件
         //@param obj  parse.posType【posType.type=='01'表示first-child;  posType.maxX 水平最大移动值 ; posType.minX 水平最小移动值 】
@@ -629,6 +686,9 @@
                 if (now.getTime() > exitTime)
                     return;
             }
+        },
+        isArray:function (obj) {
+            return Object.prototype.toString.call(obj) === 'Array';
         },
         setCookie:function (name, value) {
             var Days = 365;
